47415e62eaed23404c8af0d017f0b0a8945354b2
Soumil Shamak <rishitroyr2535@gmail.com>
2026-06-24 17:03:31 +0530

    Mockito exercise 1

---
+++ Week 1/TDD/Mockito/exercise1/src/main/java/com/example/MyService.java
@@ -0,0 +1,14 @@
+import com.example.ExternalApi;
+
+public class MyService {
+
+    private ExternalApi api;
+
+    public MyService(ExternalApi api) {
+        this.api = api;
+    }
+
+    public String fetchData() {
+        return api.getData();
+    }
+}
