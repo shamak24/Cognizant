adb2d538c06ee91ff4fe7c23fd1ec2a829ca6bef
Soumil Shamak <rishitroyr2535@gmail.com>
2026-07-10 01:26:20 +0530

    Week 4 Microservices

---
+++ Week 4/micorservices/loan/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -0,0 +1,24 @@
+package Week 4.micorservices.loan.loan.src.main.java.com.cognizant.loan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.loan.model.Loan;
+
+@RestController
+public class LoanController {
+
+    @GetMapping("/loans/{number}")
+    public Loan getLoan(
+            @PathVariable String number) {
+
+        return new Loan(
+                number,
+                "Car",
+                400000,
+                3258,
+                18);
+    }
+
+}
