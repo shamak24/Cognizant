3fbdceefc7204ea179add2b51c2289457f810b2c
Soumil Shamak <rishitroyr2535@gmail.com>
2026-07-10 00:56:48 +0530

    week 4 hello controller

---
+++ Week 3/spring-learn-REST/spring-learn-REST/src/main/java/com/cognizant/spring_learn_REST/SpringLearnRestApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_learn_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnRestApplication.class, args);
+	}
+
+}
