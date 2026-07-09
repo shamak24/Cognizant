eb56f279ef2fb6bead04fc89faa00ce324c853b0
Soumil Shamak <rishitroyr2535@gmail.com>
2026-07-10 00:43:50 +0530

    week 3 load country

--- Week 3/spring-learn/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week 3/spring-learn/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,13 +1,54 @@
 package com.cognizant.spring_learn;
 
+import java.text.SimpleDateFormat;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
+		displayCountry();
 	}
+
+	public void displayDate(){
+		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		 SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+	}
+	public void displayCountry(){
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+Country country = (Country) context.getBean("country", Country.class);
+LOGGER.debug("Country : {}", country.toString());
+
+	}
+
+}
+
+class Country{
+	private String code;
+	private String name;
+
+	public Country(){
+		System.out.println("Inside Country Constructor");
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String toString() {
+		return "Country [code=" + code + ", name=" + name + "]";
+	}
+
 
 }
