6ea9a0c73cb77b062cb5baf86eb0c1428ad32a9d
Soumil Shamak <rishitroyr2535@gmail.com>
2026-07-21 19:51:09 +0530

    Week 5 calculatorApp

---
+++ Week 5/scorecalculatorapp/src/App.jsx
@@ -0,0 +1,13 @@
+import {CalculateScore} from './components/Calculator';
+function App() {
+
+  return (
+    <>
+    <div>
+      <CalculateScore Name={"Steeve"} School={"DNV Public School"} total={284} goal={3}></CalculateScore>
+    </div>
+    </>
+  )
+}
+
+export default App
